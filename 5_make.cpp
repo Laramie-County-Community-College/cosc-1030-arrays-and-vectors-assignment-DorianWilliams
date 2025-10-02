c94f95347ffb900fe945a95d817ca5f4dfa112ee
DorianWilliams <dorianwilliams@student.lccc.wy.edu>
2025-10-02 03:23:14 +0000

    Changed the code a bit and removed herobrine

--- 5_make.cpp
+++ 5_make.cpp
@@ -5,26 +5,32 @@
 using namespace std;
 
 int main() {
-    //srand(123); // Seed the random number generator
+    srand(123); // Seed the random number generator for reproducibility
 
     // Flip a coin 500 times
-    std::vector <std::string> coinFlips;
+    std::vector<string> coinFlips;
     string coinFlipResult = "";
-    
-    // write a for loop that iternates over 500 flips and counts heads or tails in the coinFlips vector
 
+    // Loop to perform 500 flips
+    for (int i = 0; i < 500; ++i) {
+        int flip = rand() % 2; // 0 or 1
+        if (flip == 0) {
+            coinFlipResult = "Heads";
+        } else {
+            coinFlipResult = "Tails";
+        }
+        coinFlips.push_back(coinFlipResult); // Add the result to the vector
+    }
 
     // Count the number of heads
     int headsCount = 0;
-
-    //handy code to iterate through a vector.  I'll give this to you for future reference.
-    //the auto& is a nice way to have c++ declare the variable as whatever type the variable that follows is first assigned to!  In this case it will be a String, because conflips is declared that way earlier
-    for (const auto& flip : coinFlips) {  
+    for (const auto& flip : coinFlips) {
         if (flip == "Heads") {
             ++headsCount;
         }
     }
-    std::cout << "Number of Heads: " <<  headsCount << '\n';
+
+    std::cout << "Number of Heads: " << headsCount << '\n';
     std::cout << "Number of Tails: " << coinFlips.size() - headsCount << '\n';
 
     return 0;
